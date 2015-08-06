c61b20bd47d486db9ae1d6c916caeac23ea6fcf0
Allen Garvey <allengarvey@hotmail.com>
2015-08-06 18:44:00 -0400

    edit title

--- inc/views/homepage.php
+++ inc/views/homepage.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title>Test of Band.js</title>
+        <title>Earscape</title>
         <meta name="description" content="Test of band.js library"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" type="text/css" href="<?= STYLES_URL.'master.css'; ?>"/>
