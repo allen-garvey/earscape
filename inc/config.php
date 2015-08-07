0ec034d517c25028395ce2c2d03064a9146300f0
Allen Garvey <allengarvey@hotmail.com>
2015-08-07 00:34:43 -0400

    add svg_path constant to config file

--- inc/config.php
+++ inc/config.php
@@ -12,6 +12,7 @@ define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
 define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 define('MODELS_PATH', INC_PATH.'models/');
+define('SVG_PATH', INC_PATH.'svg/');
 
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
