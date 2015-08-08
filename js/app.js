2fd0fdce2e53e75c7b9b161914148a278645bd43
Allen Garvey <allengarvey@hotmail.com>
2015-08-08 01:35:27 -0400

    rename randElement function to randItem

--- js/app.js
+++ js/app.js
@@ -2,19 +2,18 @@ var toneRow = ESC.array.shuffle(ESC.notes.noteNames);
 
 function init(){
 	var conductor = new BandJS();
-	var notationText = 'tabstave notation=true tablature=false \n notes ';
+	var notationText = 'tabstave notation=true tablature=false\nnotes ';
 	conductor.setTimeSignature(4,4);
 	conductor.setTempo(120);
 	var piano = conductor.createInstrument();
 	for (var i = 0; i < toneRow.length; i++) {
 		var note = toneRow[i] + '/4';
-		var duration = ESC.array.randElement(ESC.notes.noteDurations);
+		var duration = ESC.array.randItem(ESC.notes.noteDurations);
 		notationText = notationText + ESC.notes.noteDurationsNotation[duration] + ' ' + note.replace(/b/g, '@') + ' ';
 		piano.note(duration, note.replace('/', ''));
 	};
 	var sheet_music = document.getElementById('sheet_music');
 	sheet_music.innerHTML = notationText;
-	sheet_music.className = 'vex-tabdiv';
 	console.log(notationText);
 	return conductor.finish();
 }
--- js/array.js
+++ js/array.js
@@ -7,25 +7,34 @@ ESC.array = {};
 * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 */
 ESC.array.shuffle = function(array) {
-  var currentIndex = array.length;
-  var temporaryValue, randomIndex;
+    var cloneArray = array.slice(0);
+    var currentIndex = cloneArray.length;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        var randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // And swap it with the current element.
+        var temporaryValue = cloneArray[currentIndex];
+        cloneArray[currentIndex] = cloneArray[randomIndex];
+        cloneArray[randomIndex] = temporaryValue;
+    }
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+  return cloneArray;
 }
 
-ESC.array.randElement = function(array){
+ESC.array.randItem = function(array){
 	return array[Math.floor(Math.random() * array.length)];
+}
+/*
+* Returns array of numbers from start to end exclusive (one less than the end number)
+*/
+ESC.array.range = function(start, end){
+    var a = [];
+    for (var i = start; i < end; i++) {
+        a.push(i);
+    };
+    return a;
 }
