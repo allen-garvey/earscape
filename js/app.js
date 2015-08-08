8f10f10b85b2847e7b905dd1f90e6e01c33a7a1e
Allen Garvey <allengarvey@hotmail.com>
2015-08-08 01:54:36 -0400

    refactor procedural code and change to oop models for building melodies, notation, and player

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
-var toneRow = ESC.array.shuffle(ESC.notes.noteNames);
-
 function init(){
-	var conductor = new BandJS();
-	var notationText = 'tabstave notation=true tablature=false\nnotes ';
-	conductor.setTimeSignature(4,4);
-	conductor.setTempo(120);
-	var piano = conductor.createInstrument();
-	for (var i = 0; i < toneRow.length; i++) {
-		var note = toneRow[i] + '/4';
-		var duration = ESC.array.randItem(ESC.notes.noteDurations);
-		notationText = notationText + ESC.notes.noteDurationsNotation[duration] + ' ' + note.replace(/b/g, '@') + ' ';
-		piano.note(duration, note.replace('/', ''));
-	};
+	var melodyFactory = new ESC.models.MelodyFactory();
+	var melody = melodyFactory.getToneRow();
+	
 	var sheet_music = document.getElementById('sheet_music');
-	sheet_music.innerHTML = notationText;
-	console.log(notationText);
-	return conductor.finish();
+	sheet_music.innerHTML = melody.toNotation();
+	console.log(melody.toNotation());
+	return melody.getPlayer();
 }
 
 function play(){
 	player.play();	
 }
 
-player = init();
+var player = init();
--- js/melody.js
+++
@@ -1,5 +0,0 @@
-ESC.notes = {};
-
-ESC.notes.noteNames = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
-ESC.notes.noteDurations = ['quarter', 'eighth', 'dottedQuarter', 'half', 'dottedHalf', 'sixteenth', 'dottedEighth'];
-ESC.notes.noteDurationsNotation = {'quarter': ':q', 'eighth' : ':8', 'sixteenth' : ':16', 'half' : ':h', 'dottedHalf': ':hd', 'dottedQuarter' : ':qd', 'dottedEighth' : ':8d'};
