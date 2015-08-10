084f5d0845296d2c5f60a186f270326bac519fee
Allen Garvey <allengarvey@hotmail.com>
2015-08-10 15:39:28 -0400

    add jquery and ids so buttons can be selected

--- inc/views/homepage.php
+++ inc/views/homepage.php
@@ -17,25 +17,25 @@
         </header>
         <main class='container'>
             <div class='controls'>
-                <div class='flex_container'><input type="number" class='tempo_input' value='120' /><input type="range" min='1' max='300' step='1' class='tempo_slider' /></div>
+                <div class='flex_container'><input type="number" class='tempo_input' value='120' /><input type="range" min='1' max='300' step='1' value='120' class='tempo_slider' /></div>
                 <div class='flex_container'>
                     <div class='button_container play' id='play_button'><?php include(SVG_PATH.'ei-play.svg'); ?></div>
                     <ul>
                         <li><div class='button_container' id='new_button'><?php include(SVG_PATH.'ei-plus.svg'); ?><span>New Melody</span></div></li>
-                        <li><div class='button_container'><?php include(SVG_PATH.'ei-refresh.svg'); ?><span>Change Notes</span></div></li>
-                        <li><div class='button_container'><?php include(SVG_PATH.'ei-refresh.svg'); ?><span>Change Rhythm</span></div></li>
+                        <li><div class='button_container' id='button_replace_notes'><?php include(SVG_PATH.'ei-refresh.svg'); ?><span>Change Notes</span></div></li>
+                        <li><div class='button_container' id='button_replace_rhythm'><?php include(SVG_PATH.'ei-refresh.svg'); ?><span>Change Rhythm</span></div></li>
                     </ul>
                 </div>
             </div>
             <div id='sheet_music'>
                 <canvas class="vex-canvas" id='sheet_music_canvas'  width="400" height="200"></canvas>
             </div>
-            <div class='options'>
+            <div class='transformations'>
                 <ul>
-                    <li>Original</li>
-                    <li>Inversion</li>
-                    <li>Retrograde</li>
-                    <li>Retrograde Inversion</li>
+                    <li id='button_transform_original' class='selected'>Original</li>
+                    <li id='button_transform_inversion'>Inversion</li>
+                    <li id='button_transform_retrograde'>Retrograde</li>
+                    <li id='button_transform_retrograde_inversion'>Retrograde Inversion</li>
                 </ul>
             </div>
             <div class='playlist'>
@@ -45,6 +45,7 @@
             </div>
 
         </main>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script type="text/javascript" src='<?= SCRIPTS_URL.'vextab-div.js'; ?>'></script>
         <script type="text/javascript" src='<?= SCRIPTS_URL.'band.min.js'; ?>'></script>
         <script type="text/javascript" src='<?= SCRIPTS_URL.'app.js'; ?>'></script>
