954fb814f5f8198ed6f7c92f5f5a5bf7265a64f7
Allen Garvey <allengarvey@hotmail.com>
2015-08-10 00:59:19 -0400

    refactor to use controllers instead of procedural code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-function init(){
-	var melody =  ESC.models.MelodyFactory.getToneRow();
-	
-	var sheet_music = document.getElementById('sheet_music');
-	sheet_music.innerHTML = melody.toNotation();
-	console.log(melody.toNotation());
-	return melody.getPlayer();
-}
-
-function play(){
-	player.play();	
-}
-
-var player = init();
+ESC.jukebox = new ESC.controllers.Jukebox();
+ESC.jukebox.newPlayItem();
+document.getElementById('play_button').onclick = function(){ESC.jukebox.play();};
+document.getElementById('new_button').onclick = function(){ESC.jukebox.newPlayItem();};
