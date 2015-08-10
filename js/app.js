d98300e388b353e09252a3b2417dba21e3531842
Allen Garvey <allengarvey@hotmail.com>
2015-08-10 15:39:58 -0400

    add click handlers for replace pitches, replace notes, and original and retrograde transformations

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ ESC.jukebox = new ESC.controllers.Jukebox();
 ESC.jukebox.newPlayItem();
 document.getElementById('play_button').onclick = function(){ESC.jukebox.play();};
 document.getElementById('new_button').onclick = function(){ESC.jukebox.newPlayItem();};
+
+document.getElementById('button_replace_notes').onclick = function(){ESC.jukebox.melodyWithNew('notes')};
+document.getElementById('button_replace_rhythm').onclick = function(){ESC.jukebox.melodyWithNew('rhythm')};
+
+document.getElementById('button_transform_original').onclick = function(){
+																			ESC.jukebox.transformMelody('original');
+																			$('.transformations li').removeClass('selected');
+																			$(this).addClass('selected');
+																		};
+
+
+document.getElementById('button_transform_retrograde').onclick = function(){
+																			ESC.jukebox.transformMelody('retrograde');
+																			$('.transformations li').removeClass('selected');
+																			$(this).addClass('selected');
+																			};
