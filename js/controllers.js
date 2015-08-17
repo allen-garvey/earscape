0698b427c35ac1a2059360d309c6aeb423698e64
Allen Garvey <allengarvey@hotmail.com>
2015-08-16 22:16:09 -0400

    add functionality to controller to set play list items 'selected' class

--- js/controllers.js
+++ js/controllers.js
@@ -45,15 +45,25 @@ ESC.controllers.Jukebox = function(){
 }
 ESC.controllers.Jukebox.prototype.addPlayItem = function(playItem){
 	this.playItems.push(playItem);
-	this.setCurrentPlayItem(this.playItems.length - 1);
 	var play_items_list = document.getElementById('play_items_list');
-	console.log(playItem.getTitle());
+	// console.log(playItem.getTitle());
 	play_items_list.innerHTML = play_items_list.innerHTML + "<li>" + playItem.getTitle() + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M15.2 40.6c-.2 0-.4-.1-.6-.2-.4-.3-.5-.7-.4-1.1l3.9-12-10.2-7.5c-.4-.3-.5-.7-.4-1.1s.5-.7 1-.7h12.7L25 5.9c.1-.4.5-.7 1-.7s.8.3 1 .7L30.9 18h12.7c.4 0 .8.2 1 .6s0 .9-.4 1.1L34 27.1l3.9 12c.1.4 0 .9-.4 1.1s-.8.3-1.2 0L26 33l-10.2 7.4c-.2.1-.4.2-.6.2zM26 30.7c.2 0 .4.1.6.2l8.3 6.1-3.2-9.8c-.1-.4 0-.9.4-1.1l8.3-6.1H30.1c-.4 0-.8-.3-1-.7L26 9.5l-3.2 9.8c-.1.4-.5.7-1 .7H11.5l8.3 6.1c.4.3.5.7.4 1.1L17.1 37l8.3-6.1c.2-.1.4-.2.6-.2z"/></svg>' +  "</li>";
+	this.setCurrentPlayItem(this.playItems.length - 1);
+	//scroll to added item
+	var playlist = $('.playlist');
+	playlist.scrollTop(playlist[0].scrollHeight);
 }
 
 ESC.controllers.Jukebox.prototype.setCurrentPlayItem = function(index){
 	this.currentPlayItemIndex = index;
 	this.currentPlayItem = this.playItems[index];
+	
+	//sets selected state in play_items_list
+	var allPlaylistItems = $('#play_items_list li');
+	allPlaylistItems.removeClass('selected');
+	allPlaylistItems.eq(index).addClass('selected');
+
+	//displays sheet music
 	this.displayCurrentPlayItem();
 }
 ESC.controllers.Jukebox.prototype.displayCurrentPlayItem = function(){
