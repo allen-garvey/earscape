2f0e86e9aa946d8382cfe453d075a054e33ea821
Allen Garvey <allengarvey@hotmail.com>
2015-08-21 20:20:20 -0400

    refactor to only minify and add vendor js libraries into main app.js file in production environments to speed gulp compilation times during development

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,14 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var maps = require('gulp-sourcemaps');
 
+var appSourceFiles = ['js/init.js', 'js/array.js', 'js/models.js', 'js/controllers.js', 'js/app.js'];
+var vendorSourceFiles = ['js/vendor/bandjs/band.min.js', 'js/vendor/vextab/vextab-div.js'];
+
+/*
+* tasks to build for production
+*/
 gulp.task('concatScripts', function(){
-	return gulp.src(['js/vendor/bandjs/band.min.js', 'js/vendor/vextab/vextab-div.js', 'js/init.js', 'js/array.js', 'js/models.js', 'js/controllers.js', 'js/app.js'])
+	return gulp.src(vendorSourceFiles.concat(appSourceFiles))
 		.pipe(maps.init())
 		.pipe(concat('app.js'))
 		.pipe(maps.write('./'))
@@ -20,9 +26,28 @@ gulp.task('minifyScripts', ['concatScripts'], function(){
 		.pipe(rename('app.min.js'))
 		.pipe(gulp.dest('scripts'));
 });
-gulp.task('watchScripts', function(){
-	gulp.watch('js/**/*.js', ['minifyScripts']);
+
+/*
+* tasks to build for development since it is so slow to compile vendor source files
+*/
+gulp.task('concatScriptsDev', function(){
+	return gulp.src(appSourceFiles)
+		.pipe(maps.init())
+		.pipe(concat('app.js'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('scripts'));
 });
 
+/*
+* Tasks to build or watch for development only
+*/
+gulp.task('watchScripts', function(){
+	gulp.watch('js/**/*.js', ['concatScriptsDev']);
+});
+gulp.task('buildDev', ['concatScriptsDev']);
+
+/*
+* Default is to build for production
+*/
 gulp.task('build', ['minifyScripts']);
 gulp.task('default', ['build']);
--- inc/views/homepage.php
+++ inc/views/homepage.php
@@ -50,6 +50,12 @@
             <a href="http://www.github.com/allen-garvey/earscape">Source on GitHub</a>
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src='<?= SCRIPTS_URL.'app.js'; ?>'></script>
+        <?php if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT): ?>
+            <script type="text/javascript" src='<?= SCRIPTS_URL.'band.min.js'; ?>'></script>
+            <script type="text/javascript" src='<?= SCRIPTS_URL.'vextab-div.js'; ?>'></script>
+            <script type="text/javascript" src='<?= SCRIPTS_URL.'app.js'; ?>'></script>
+        <?php else: ?>
+            <script type="text/javascript" src='<?= SCRIPTS_URL.'app.min.js'; ?>'></script>
+        <?php endif; ?>
     </body>
 </html>
