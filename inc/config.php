928a995b14f13bcb15df59b55f641ad7e7d38889
Allen Garvey <allengarvey@hotmail.com>
2016-02-16 10:15:59 -0500

    remove unused files and directories

--- inc/config.php
+++ inc/config.php
@@ -7,14 +7,15 @@ if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT){
 	define('BASE_URL','/earscape/');
 	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/earscape/');
 }
+else{
+	define('BASE_URL','');
+	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/earscape/');	
+}
 
 define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
-define('CONTROLLERS_PATH', INC_PATH.'controllers/');
-define('MODELS_PATH', INC_PATH.'models/');
 define('SVG_PATH', INC_PATH.'svg/');
 
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
-define('IMAGES_URL', BASE_URL.'images/');
 
